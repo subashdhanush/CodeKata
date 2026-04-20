7bced05d0b678dafc2c5fd6e3fe30a003fed6100
subashdhanush <85820233+subashdhanush@users.noreply.github.com>
2026-04-20 16:03:49 +0530

    Add files via upload

---
+++ removeextraspace.py
@@ -0,0 +1,25 @@
+s = input()
+result = " ".join(s.split())
+print(result)
+
+
+# Problem Statement:
+# Given a sentence and string S, find how many times S occurs in the given sentence.If S is not found in the sentence print -1
+
+
+# Input Description:
+# Input Size : |sentence| <= 1000000(complexity O(n)).
+
+
+# Output Description:
+# The output is the number of times S occurs in the given sentence, or -1 if S is not found.
+
+
+# Sample Input:
+# I enjoy doing codekata
+# codekata
+
+
+# Sample Output:
+# 1
+
