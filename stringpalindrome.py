56391033ddb637a441e438513b6f111b2a2434b0
subashdhanush <85820233+subashdhanush@users.noreply.github.com>
2026-04-21 15:53:00 +0530

    Add files via upload

---
+++ stringpalindrome.py
@@ -0,0 +1,17 @@
+name=input()
+result=name[::-1]
+if name == result:
+    print("yes")
+else:
+    print("no")
+
+# Problem Statement:
+# Given a string S, print 'yes' if it is a palindrome or 'no' if it is not a palindrome.
+
+
+# Sample Input:
+# lappal
+
+
+# Sample Output:
+# yes    
