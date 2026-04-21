42e4ace440e1db88e079b7ddd906688062f13af0
subashdhanush <85820233+subashdhanush@users.noreply.github.com>
2026-04-21 15:52:38 +0530

    Add files via upload

---
+++ stringlengthwithoutfunction.py
@@ -0,0 +1,17 @@
+name=input()
+count=0
+for i in name:
+    count=count+1
+print(count)    
+
+
+# Problem Statement:
+# Given a string S, find its length(including the spaces)without using any pre-defined functions.
+
+
+# Sample Input:
+# codekata
+
+
+# Sample Output:
+# 8
